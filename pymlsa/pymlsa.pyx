20aa3ce2c4836159a0707150a2f51fb6b0c1e038
Kei-ti <kou.tanaka.4research@gmail.com>
2022-03-07 05:13:22 +0000

    update

--- pymlsa/pymlsa.pyx
+++ pymlsa/pymlsa.pyx
@@ -6,6 +6,70 @@ from libc.string cimport memcpy, memset
 from cpython cimport PyObject, Py_INCREF, Py_DECREF
 
 
+def mcepalpha(fs, start=0.0, stop=1.0, step=0.001, num_points=1000):
+    """Compute appropriate frequency warping parameter given a sampling frequency
+
+    It would be useful to determine alpha parameter in mel-cepstrum analysis.
+
+    The code is traslated from https://bitbucket.org/happyalu/mcep_alpha_calc.
+
+    Parameters
+    ----------
+    fs : int
+        Sampling frequency
+
+    start : float
+        start value that will be passed to numpy.arange. Default is 0.0.
+
+    stop : float
+        stop value that will be passed to numpy.arange. Default is 1.0.
+
+    step : float
+        step value that will be passed to numpy.arange. Default is 0.001.
+
+    num_points : int
+        Number of points used in approximating mel-scale vectors in fixed-
+        length.
+
+    Returns
+    -------
+    alpha : float
+        frequency warping paramter (offen denoted by alpha)
+
+    See Also
+    --------
+    pysptk.sptk.mcep
+    pysptk.sptk.mgcep
+
+    """
+    
+    def _melscale_vector(fs, length):
+        step = (fs / 2.0) / length
+        melscalev = 1000.0 / np.log(2) * np.log(1 + step * np.arange(0, length) / 1000.0)
+        return melscalev / melscalev[-1]
+    
+    def _warping_vector(alpha, length):
+        step = np.pi / length
+        omega = step * np.arange(0, length)
+        num = (1 - alpha * alpha) * np.sin(omega)
+        den = (1 + alpha * alpha) * np.cos(omega) - 2 * alpha
+        warpfreq = np.arctan(num / den)
+        warpfreq[warpfreq < 0] += np.pi
+        return warpfreq / warpfreq[-1]
+    
+    def _rms_distance(v1, v2):
+        d = v1 - v2
+        return np.sum(np.abs(d * d)) / len(v1)
+
+    alpha_candidates = np.arange(start, stop, step)
+    mel = _melscale_vector(fs, num_points)
+    distances = [
+        _rms_distance(mel, _warping_vector(alpha, num_points))
+        for alpha in alpha_candidates
+    ]
+    return alpha_candidates[np.argmin(distances)]
+
+
 cdef void freqt(const double *c, double *wc, double *prev, int c_size, double alpha, int order) nogil:
     cdef int i, j
     for i in range(-(c_size - 1), 1):  # -(c_size - 1) 〜 0
