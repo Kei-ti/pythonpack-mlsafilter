6f2f05aa0af4a73bc6453d9fe9f8d2ed478e299f
田中 宏 <tanaka.ko@navy301.cnet>
2025-12-12 11:38:25 +0900

    update

--- setup.py
+++ setup.py
@@ -10,31 +10,28 @@ from os.path import join
 
 from setuptools.command.build_ext import build_ext as _build_ext
 
-#DOCLINES = __doc__.split('\n')
+import builtins  # ★追加
+
 _VERSION = '0.1'
 
 class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        builtins.__NUMPY_SETUP__ = False  # ★修正（__builtins__ ではなく builtins）
         import numpy
         self.include_dirs.append(numpy.get_include())
-
-#mlsa_src_top = "lib"
-#mlsa_sources = glob(join(mlsa_src_top, "*.cpp"))
 
 ext_modules = [
     Extension(
         name="pymlsa",
-        #include_dirs=[mlsa_src_top],
-        sources=[join("pymlsa", "pymlsa.pyx")], #+ mlsa_sources,
-        language="c++")]
+        sources=[join("pymlsa", "pymlsa.pyx")],
+        language="c++"
+    )
+]
 
 setup(
     name="pymlsa",
-    #description=DOCLINES,
-    #long_description='\n'.join(DOCLINES[2:]),
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
     version=_VERSION,
